e57b37b3fb53afe6e4abd3b0c96ce1fbef10a87a
ohnequatschdeals <n.goekmen@ohnequatschdeals.de>
2025-09-04 07:19:56 +0000

    feat: landing (Hero + Offers + TopBerater), DS utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,3 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-
-/**
- * Tailwind className merge utility.
- * Example: cn("px-2", condition && "bg-red-500")
- */
-export function cn(...inputs: any[]) {
-  return twMerge(clsx(inputs));
-}
+export function cn(...i:any[]){ return twMerge(clsx(i)); }
